978abe84293b4d29aae4d69a0b41780fea32ef3b
Muhammad Haiqal Dani <51081082+haiqaldani@users.noreply.github.com>
2022-04-22 17:53:54 +0000

    Make order model

--- spec/factories/order_details.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :order_detail do
-    order_id { 1 }
-    menu_id { 1 }
-    price { 1.5 }
-    1uantity { 1 }
-  end
-end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     customer_name { "MyString" }
     customer_email { "MyString" }
     status { "MyString" }
-    order_date { "2022-04-22 17:40:36" }
+    order_date { "2022-04-22 17:53:40" }
   end
 end
